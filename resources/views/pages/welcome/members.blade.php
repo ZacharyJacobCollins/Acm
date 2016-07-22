e9cf2daf72592173f93f6ddca26f2647279102da
zachary collins <zacharyjacobcollins@gmail.com>
2016-07-22 00:11:19 -0400

    added in columns

--- resources/views/pages/welcome/members.blade.php
+++ resources/views/pages/welcome/members.blade.php
@@ -15,26 +15,25 @@
 
  
         <div class="container">
-            <!-- Members container --> 
-                <!-- Iterate through members producing a card for each --> 
+            <div class="memberCardContainer">
+                <!-- Members container --> 
                     @for($i=1; $i < count($members); $i++)
-                            <div class="section">  
-                                <!-- Card -->
-                                <div class="card black col s1 ">
-                                    <div class="card-image">
-                                        <img class="activator" src="/images/avatars/{{ $members[$i]->avatar }}">
-                                    </div>
-                                    <div class="card-content">
-                                        <span class="card-title activator grey-text text-darken-4">{{ $members[$i]->name }}<i class="material-icons right">more_vert</i></span>
-                                        <p><a href="#">{{ $members[$i]->languages }}</a></p>
-                                    </div>
-                                    <div class="card-reveal">
-                                        <span class="card-title grey-text text-darken-4">{{ $members[$i]->name }}<i class="material-icons right">close</i></span>
-                                        <p>{{ $members[$i]->bio }}</p>
-                                    </div>
-                                </div>
+                        <!-- Card -->
+                        <div class="card black memberCard col s1">
+                            <div class="card-image">
+                                <img class="activator" src="/images/avatars/{{ $members[$i]->avatar }}">
                             </div>
+                            <div class="card-content">
+                                <span class="card-title activator grey-text text-darken-4">{{ $members[$i]->name }}<i class="material-icons right">more_vert</i></span>
+                                <p><a href="#">{{ $members[$i]->languages }}</a></p>
+                            </div>
+                            <div class="card-reveal">
+                                <span class="card-title grey-text text-darken-4">{{ $members[$i]->name }}<i class="material-icons right">close</i></span>
+                                <p>{{ $members[$i]->bio }}</p>
+                            </div>
+                        </div>
                     @endfor
+            </div>
         </div>
     
    @include('layouts.footer')
